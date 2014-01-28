d21869271cee048edd4a31bae29d1cfb78cbbd51
DeanStark <gotoxtreme@gmail.com>
2014-01-29 04:37:21 +0900

    First Commit

---
+++ src/main/java/com/ssm/quadrah/quadrah_api_server/App.java
@@ -0,0 +1,13 @@
+package com.ssm.quadrah.quadrah_api_server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
